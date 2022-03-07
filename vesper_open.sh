8f11919ecc20cd412e7c0474f4d9189a62cfdc3f
Richard Littauer <richard+github@burntfen.com>
2022-03-07 10:42:51 +0000

    Modify vesper options for current setup

--- vesper_open.sh
+++ vesper_open.sh
@@ -1,6 +1,14 @@
 #! /bin/sh
 
-cd /Volumes/Erebus/msgr-vesper-archive/
-conda activate vesper-0.4.8
+cd /Volumes/Chaffinch/
+python3 ~/src/vesper/vesper-audio-shim/rename_audiomoth_recordings.py
+open .
+#
+# cp *.wav
+
+#cd /Volumes/Erebus/msgr-vesper-archive/
+cd /Users/richard/Sound/edinburgh-nocmig/Recordings
+open .
+conda activate vesper-d603294
 open http://127.0.0.1:8000/
 vesper_admin runserver
