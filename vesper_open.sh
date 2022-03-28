71be22157e5cc64fa0b7cfbb42b0fac8c371164c
Richard Littauer <richard+github@burntfen.com>
2022-03-28 17:44:20 +0100

    Change process to eliminate cp locally

--- vesper_open.sh
+++ vesper_open.sh
@@ -1,14 +1,8 @@
 #! /bin/sh
 
 cd /Volumes/Chaffinch/
-python3 ~/src/vesper/vesper-audio-shim/rename_audiomoth_recordings.py
-open .
-#
-# cp *.wav
-
-#cd /Volumes/Erebus/msgr-vesper-archive/
-cd /Users/richard/Sound/edinburgh-nocmig/
-open .
+python3 ~/src/vesper-audio-shim/rename_audiomoth_recordings.py
+cd /Users/richard/Sound/nocmig-blebo-craig/
 conda activate vesper-d603294
 open http://127.0.0.1:8000/
 vesper_admin runserver
