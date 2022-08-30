e166ba931cf6b7906ac520f1319803a839e3fcee
Richard Littauer <richard+github@burntfen.com>
2022-08-30 11:11:23 +0200

    Small changes

--- vesper_open.sh
+++ vesper_open.sh
@@ -1,11 +1,15 @@
 #! /bin/sh
 
+RECORDINGS="/Users/richard/Sound/nfc-22-04-16-oich/Recordings"
+
+# Note: You need to copy the archive template and instantiate Conda, and I'm not sure this works all of the time
+
 # Edit this if you want to import only a subfolder
-cd /Volumes/Chaffinch/
+cd /Volumes/Crossbill/April\ 16\ Bridge\ of\ Oich/
 # Edit these lines to change to the right place
-python3 ~/src/vesper-audio-shim/rename_audiomoth_recordings.py "Edinburgh" /Users/richard/Sound/edinburgh-nocmig/Recordings
-cd /Users/richard/Sound/edinburgh-nocmig/
+python3 ~/src/vesper-audio-shim/rename_audiomoth_recordings.py "Bridge of Oich" $RECORDINGS
+cd $RECORDINGS
 # Edit this to the most recent commit
-conda activate vesper-2470db0
+conda activate vesper-0.4.13
 open http://127.0.0.1:8000/
 vesper_admin runserver
