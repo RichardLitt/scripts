88458fe1cd358c600cf4efbf175683ff402b33a4
Richard Littauer <richard+github@burntfen.com>
2022-08-30 11:10:10 +0200

    Adding another hotspot and comments

--- ebird-hotspots/getids.sh
+++ ebird-hotspots/getids.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# To run this file, move it into an eBird export folder and run it.
+# Note: You also need the 10k ids file in that folder.
+# TODO Make it accessible from anywhere, or add an input to file.
+
 function makecleanfile(){
   cat clean.txt | head -n1 > montclean.txt
   while read line; do
